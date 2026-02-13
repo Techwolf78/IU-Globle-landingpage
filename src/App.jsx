ff8a44cbf6e4e0850099462c68f4e2e6b321292c
Dipratna Mahire <dipratnamahire2222@gmail.com>
2026-02-13 18:32:27 +0530

    ok

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,7 @@ import Footer from "./Pages/Footer";
 import Brochure from "./Pages/Brochure";
 import BbaAdmission from "./Pages/BbaAdmission";
 import ApplyButtonRight from "./Components/ApplyButtonRight";
-
-
+import ThankYou from "./Pages/Thankyou";
 // Importing the 404 Page
 import NotFoundPage from "./Components/NotFoundPage"; // Make sure this is the correct path to your 404 page
 
@@ -22,6 +21,7 @@ function App() {
             {/* Define routes for all the pages */}
             <Route path="/" element={<BbaAdmission />} />
             <Route path="*" element={<NotFoundPage />} />
+            <Route path="/thankyou" element={<ThankYou />} />
           </Routes>
           <Footer />
         </main>
